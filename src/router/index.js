be308674fe90f77c8348b0d7eef2a3bc834b40d5
pneff <pneff@unicon.net>
2018-01-10 16:50:18 -0700

    first working

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PrivateBattles from '@/components/privateBattles'
+import PublicBattles from '@/components/publicBattles';
+
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+      name: 'PublicBattles',
+      component: PublicBattles,
+    },
+    {
+      path: '/private-battles',
+      name: 'PrivateBattles',
+      component: PrivateBattles,
+    },
+  ],
+});
---
+++ src/utils/battles-api.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8081';
+
+export {getPublicStartupBattles, getPrivateStartupBattles};
+
+function getPublicStartupBattles() {
+  const url = `${BASE_URL}/api/battles/public`;
+  return axios.get(url).then(response => response.data);
+}
+
+function getPrivateStartupBattles() {
+  const url = `${BASE_URL}/api/battles/private`;
+  return axios.get(url).then(response => response.data);
+}
